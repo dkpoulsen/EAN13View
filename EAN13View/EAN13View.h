5b33bcf4a7dca87df9962cb836387a025e0f3b50
Daniel K. Poulsen <dkpoulsen@me.com>
2019-09-03 17:22:32 +0200

    Initial Commit

---
+++ EAN13View/EAN13View.h
@@ -0,0 +1,19 @@
+//
+//  EAN13View.h
+//  EAN13View
+//
+//  Created by Daniel K. Poulsen on 03/09/2019.
+//  Copyright © 2019 Daniel K. Poulsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EAN13View.
+FOUNDATION_EXPORT double EAN13ViewVersionNumber;
+
+//! Project version string for EAN13View.
+FOUNDATION_EXPORT const unsigned char EAN13ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EAN13View/PublicHeader.h>
+
+
